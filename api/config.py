1e45dc4c059c3df9f136d5488c6d038fd432bfe5
SELMImohamed <91205436+SELMImohamed@users.noreply.github.com>
2023-01-26 10:36:36 +0100

    change key

--- api/config.py
+++ api/config.py
@@ -1,8 +1,8 @@
 from mysql.connector import connect
 
 db = connect(
-    host="localhost",
-    port="8889",
+    host="192.168.64.2",
+    port="3306",
     user="root",
     password="root",
     database="linux_archi",
